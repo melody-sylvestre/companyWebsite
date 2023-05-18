9e1bfb7dbd319bed8df0a34c10f2e5558ec792d7
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-05-18 18:59:05 +0100

    correct footer layout

--- company-app/src/Footer/Footer.js
+++ company-app/src/Footer/Footer.js
@@ -1,6 +1,9 @@
+import "./Footer.scss"
 const Footer = () => {
     return (
-        <></>
+        <footer>
+            <p>Website developed by <a href="https://www.avamae.co.uk/">AVAMAE</a></p>
+        </footer>
     )
 }
 export default Footer
