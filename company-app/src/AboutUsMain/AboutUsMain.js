718e5c30d3b209f93b0e5361f3ad1fd06845ad8d
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-05-21 19:32:12 +0100

    About Us page with layout but without styling

---
+++ company-app/src/AboutUsMain/AboutUsMain.js
@@ -0,0 +1,23 @@
+import "./AboutUsMain.scss"
+const AboutUsMain = () => {
+    return (
+        <main className="about_us_main">
+            <h1>About us</h1>
+            <p className="about_us_subtitle"></p>
+            <p>Et sunt nobis in reprehenderit sunt cum internos omnis eum saepe molestiae sed dolorem consequatur qui temporibus voluptate. Id corporis architecto nam sunt voluptas sed quas mollitia et culpa deserunt. Eum rerum cupiditate ut quis omnis qui rerum nemo ut quas libero eum animi nemo. </p>
+            <p>Et consequatur magnam ut ratione obcaecati sed eius amet. At libero laborum et doloremque ratione et rerum aperiam ut amet iure. Ut expedita consectetur aut exercitationem dolor est blanditiis impedit aut ipsum enim vel internos expedita sed voluptatem modi. Qui modi esse aut officiis internos in molestias deleniti cum consectetur quam cum facilis beatae aut debitis modi? </p>
+            <img className="image_about_us" src="images/shutterstock_696636415.jpg" alt="An open space office with a breakroom section" />
+            <p>Lorem ipsum dolor sit amet. Et perferendis quod ex laboriosam praesentium aut galisum alias eos quisquam quaerat 33 dolorem natus ut aliquam doloremque. In esse iusto eum aperiam error rem natus ipsum. Qui quae tempore 33 maiores recusandae aut blanditiis quidem eum odit excepturi qui laborum minus ex vitae deserunt.</p>
+            <h2>Sit quibusdam quaerat hic omnis</h2>
+            <ul>
+                <li>Et exercitationem similique et eligendi rerum</li>
+                <li>Hic reiciendis laboriosam aut officia mollitia</li> <li>Sit iure explicabo ut atque culpa et officiis</li> <li>Sint sit unde galisum qui voluptatem labore est</li> 
+                </ul>
+            <p>Eos cumque fugit et placeat delectus est quae corporis sed obcaecati alias. Hic incidunt assumenda aut velit dolor non sint dolor in ipsam quia nam vitae fugit et rerum dolorem. </p>
+        </main>
+
+
+    )
+}
+
+export default AboutUsMain 
