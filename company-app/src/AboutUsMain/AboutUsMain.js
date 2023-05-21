4d1a788f32a919dd95583a505558e21e113efb25
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-05-21 19:46:52 +0100

    basic styling of AboutUsPage

--- company-app/src/AboutUsMain/AboutUsMain.js
+++ company-app/src/AboutUsMain/AboutUsMain.js
@@ -3,8 +3,8 @@ const AboutUsMain = () => {
     return (
         <main className="about_us_main">
             <h1>About us</h1>
-            <p className="about_us_subtitle"></p>
-            <p>Et sunt nobis in reprehenderit sunt cum internos omnis eum saepe molestiae sed dolorem consequatur qui temporibus voluptate. Id corporis architecto nam sunt voluptas sed quas mollitia et culpa deserunt. Eum rerum cupiditate ut quis omnis qui rerum nemo ut quas libero eum animi nemo. </p>
+            <p><span className="about_us_subtitle">Vel labore laudantium ut assumenda accusamus aut repudiandae perferendis ut galisum temporibus et maiores odio sit nulla sunt. </span><br/>
+            Et sunt nobis in reprehenderit sunt cum internos omnis eum saepe molestiae sed dolorem consequatur qui temporibus voluptate. Id corporis architecto nam sunt voluptas sed quas mollitia et culpa deserunt. <a href="#">Eum rerum cupiditate</a> ut quis omnis qui rerum nemo ut quas libero eum animi nemo. </p>
             <p>Et consequatur magnam ut ratione obcaecati sed eius amet. At libero laborum et doloremque ratione et rerum aperiam ut amet iure. Ut expedita consectetur aut exercitationem dolor est blanditiis impedit aut ipsum enim vel internos expedita sed voluptatem modi. Qui modi esse aut officiis internos in molestias deleniti cum consectetur quam cum facilis beatae aut debitis modi? </p>
             <img className="image_about_us" src="images/shutterstock_696636415.jpg" alt="An open space office with a breakroom section" />
             <p>Lorem ipsum dolor sit amet. Et perferendis quod ex laboriosam praesentium aut galisum alias eos quisquam quaerat 33 dolorem natus ut aliquam doloremque. In esse iusto eum aperiam error rem natus ipsum. Qui quae tempore 33 maiores recusandae aut blanditiis quidem eum odit excepturi qui laborum minus ex vitae deserunt.</p>
