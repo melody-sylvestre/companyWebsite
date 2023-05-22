ac401eb485a2075d110d09b9b31c67a66f706261
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-05-22 10:41:55 +0100

    fixed the link at the bottom of PlainTextCard

--- company-app/src/PlainTextCard/PlainTextCard.js
+++ company-app/src/PlainTextCard/PlainTextCard.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import "./PlainTextCard.scss"
 
-const PlainTextCard = () =>  {
+const PlainTextCard = () => {
     return (
         <div className="plain_text_card">
             <h2>Est voluptate pariatur est magnam </h2>
@@ -11,8 +11,10 @@ const PlainTextCard = () =>  {
                 <p>Aut minus debitis non iste beatae ut voluptates fugiat ea voluptas accusantium non sunt excepturi. Aut quisquam fugiat in corporis dolor qui internos suscipit vel exercitationem minus ut laboriosam nobis. In porro quis ad quia ullam rem magni accusantium eos unde culpa ut officiis sequi? Et enim laboriosam et nesciunt eaque et galisum eius sit voluptatem saepe ut inventore dicta 33 amet laborum id dolorem culpa. Est quae recusandae ut delectus cumque et accusamus quaerat ea dolorum veniam qui enim minus et eligendi magni? Et officia illo quo aliquid quos vel sapiente saepe rem internos dignissimos et enim beatae. Et deserunt recusandae nam mollitia magni aut sint rerum 33 autem voluptatem sed maiores sunt aut provident molestias.Et deserunt recusandae nam mollitia magni aut sint rerum 33 autem voluptatem sed maiores sunt aut provident molestias. Vel minus officia ex omnis omnis est minima. </p>
                 <p>In voluptatum ipsum sit impedit dolores et consequatur iure et ducimus voluptatem eos deserunt dolores qui magni culpa! Ea corporis ullam eum iusto reiciendis et sunt iusto aut voluptatem rerum eum voluptatem odit. A fugit ratione quo minima omnis eum quia officiis ex cumque natus et voluptates Quis aut quia autem. Id molestiae deleniti est modi ipsum sit sunt eaque id laudantium eius et atque natus ut repellendus aliquid. Vel minus officia ex omnis omnis est minima nostrum rem perferendis reprehenderit ut doloremque voluptatem. Eos inventore atque ut temporibus recusandae ut quaerat harum rem voluptates tempore aut autem dolorum. Eos aperiam facilis qui voluptatem alias ad nulla veniam id aliquam repellat a nesciunt dolorem id beatae velit. Et reiciendis libero in adipisci ipsa 33 officiis internos ea laborum eveniet. Qui deleniti omnis hic Quis accusamus ut accusamus sunt in ducimus blanditiis ab ullam fuga. </p>
             </div>
-            <Link className="link_plain_text_container" to="/contact-us">Contact us</Link>
+            <div className="link_plain_text_container">
+                <Link className="link_plain_text" to="/contact-us">Contact us</Link>
+            </div>
         </div>
-    )   
+    )
 }
 export default PlainTextCard
